aaecdd9cfa9773a45bf2958c817182b510fefca3
Cleberson F R Junior <cleberson.dev@gmail.com>
2022-07-16 15:58:24 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <header className="App-header">
+      <h1>Budget Planner</h1>
+      <small>because i dont wanna spend on mobills premium</small>
+    </header>
+  );
+}
+
+export default App;
