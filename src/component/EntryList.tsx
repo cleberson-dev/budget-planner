763c35e3a07f42ab1dce9b6f6cca0eb26d4225ab
Cleberson F R Junior <cleberson.dev@gmail.com>
2022-07-17 00:15:26 -0300

    Finish EntryList

--- src/component/EntryList.tsx
+++ src/component/EntryList.tsx
@@ -1,7 +1,16 @@
 import styled from "styled-components"
+import ptBR from "date-fns/locale/pt-BR"
+import format from "date-fns/format"
 
 type EntryListProps = {
   list: Entry[]
+}
+
+function getColorByEntryType(entryType: EntryTypes) {
+  return {
+    INCOME: "#2b9348",
+    EXPENSE: "#ef233c",
+  }[entryType]
 }
 
 const Container = styled.ul`
@@ -16,6 +25,11 @@ const EntryItem = styled.li`
   border-radius: 0.5rem;
   display: flex;
   align-items: center;
+  cursor: pointer;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.12);
+  }
 
   :not(:last-child) {
     margin-bottom: 1rem;
@@ -27,10 +41,27 @@ const EntryBullet = styled.div<{ type: EntryTypes }>`
   width: 2rem;
   height: 2rem;
   border-radius: 50%;
-  background: ${({ type }) =>
-    ({ INCOME: "#2b9348", EXPENSE: "#ef233c" }[type])};
+  background: ${({ type }) => getColorByEntryType(type)};
   opacity: 0.6;
   margin-right: 0.8rem;
+`
+
+const Description = styled.p`
+  margin: 0;
+`
+
+const Details = styled.small`
+  color: #cbcbcb;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+`
+
+const Price = styled.p<{ type: EntryTypes }>`
+  font-weight: 600;
+  font-size: 1rem;
+  color: ${({ type }) => getColorByEntryType(type)};
 `
 
 const EntryList = ({ list }: EntryListProps): JSX.Element => {
@@ -39,7 +70,21 @@ const EntryList = ({ list }: EntryListProps): JSX.Element => {
       {list.map((entry) => (
         <EntryItem key={entry.id}>
           <EntryBullet type={entry.type} />
-          <span>{entry.description}</span>
+          <Main>
+            <Description>{entry.description}</Description>
+            <Details>
+              {`${format(entry.createdAt, `d 'de' LLLL 'de' yyyy`, {
+                locale: ptBR,
+              })} - Carteira`}
+            </Details>
+          </Main>
+          <Price type={entry.type}>
+            {{ INCOME: "+", EXPENSE: "-" }[entry.type]} R$
+            {entry.value.toLocaleString("pt-BR", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
+          </Price>
         </EntryItem>
       ))}
     </Container>
