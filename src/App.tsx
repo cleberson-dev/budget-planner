c016f6f24a7c58b1e21236d785c9ee4764bfafe9
Cleberson F R Junior <cleberson.dev@gmail.com>
2024-02-16 03:00:44 -0300

    Customize scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,12 @@ const Titlebar = styled.div`
   user-select: none;
   -webkit-user-select: none;
   -webkit-app-region: drag;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 30px;
-  background-color: rgb(0, 0, 0, 0.2);
+  background-color: #20232a;
 `
 
 function App() {
